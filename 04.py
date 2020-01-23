9532501a7a03fd6f1d8d9dd76ccf6d18e3029b99
Раїса Кулаковська <kulakovska@rivne.itstep.org>
2020-01-23 21:01:47 +0200

    21.01.20 +

---
+++ 04.py
@@ -0,0 +1,112 @@
+import copy
+
+""" arr = [1, 42, 16, 8]
+print(arr)
+arr.append(28)
+print(arr)
+arr.insert(2, 10500)
+print(arr)
+arr.remove(10500)
+print(arr)
+arr.clear()
+print(arr)
+arr.append(55)
+arr.append(65)
+arr.append(75)
+arr.append(85)
+print(arr)
+# print(arr.index(100500))
+arr.pop()
+print(arr)
+arr.pop(1)
+print(arr)
+arr.append(8)
+arr.append("Microsoft")
+arr.append(8)
+arr.insert(1,8)
+print(arr)
+print(arr.count(8))
+#print(arr.sort())
+arr.reverse()
+arr.remove("Microsoft")
+print(arr)
+print(len(arr))
+print(min(arr))
+print(max(arr))
+
+for x in arr:
+    print(x)
+
+names = ["Bob", "Uitny", "Lussy", "Dan", "Alise", "Kite"]
+names.sort()
+print(names)
+for i in names:
+    print (i)
+
+names1 = ["Tom", "Bob", "Uitny", "Lussy", "Dan", "Alise", "Kite"]
+#names2 = names1
+names2 = copy.deepcopy(names1)
+print("names1: ", names1)
+print("names2: ", names2)
+names2.append("Stive")
+print("names1: ", names1)
+print("names2: ", names2)
+
+names3 = names1[3:5]
+print("names3: ", names3)
+names3 = names1[:2]
+print("names3: ", names3)
+names3 = names1[3:5:2]
+print("names3: ", names3)
+
+"""
+# -------1---------
+num = [5, 75, 82, 2, 15, 1, 14]
+print(num)
+min_value = min(num)
+max_value = max(num)
+min_index = num.index(min_value)
+max_index = num.index(max_value)
+# temp = copy.deepcopy(max)
+# min = temp
+# max = min
+print("min_value", min_value)
+print("max_value", max_value)
+print("min_index", min_index)
+print("max_index", max_index)
+temp = num[min_index]
+num[min_index] = num[max_index]
+num[max_index] = temp
+
+print("New Arr = ", num)
+
+# -------2---------
+num1 = [5, 75, 82, 2, 15, 1, 14]
+summ_p = 0
+summ_np = 0
+for i in num1:    
+    if i % 2 == 1:
+        summ_p += i
+    else:
+        summ_np += i    
+print("summ_p: ", summ_p)
+print("summ_np: ", summ_np)
+print(sum(num1[1::2]))
+
+
+# # -------3v---------
+# num2 = [1, 2, 3, 4, 5]
+# num3 = copy.deepcopy(num2)
+# print("num2: ", num2)
+# print("num3: ", num3)
+
+# # -------4---------
+# num4 = [11, 12, 13, 14, 15]
+# num5 = [16, 17, 18, 19, 20]
+# num6 = []
+# num6.extend(num4)
+# print(num6) 
+
+
+
+
---
+++ 05.py
@@ -0,0 +1,41 @@
+# names1 = ['Amir', 'Barry', 'Chales', 'Dao']
+# if 'amir' in names1:
+#     print(1)
+# else:
+#     print(2)
+
+# x = 4.5 
+# y = 2 
+# print(x//y) 
+
+# one = chr(104) 
+# two = chr(105) 
+# print(one + two)
+ 
+#-------------6?-------------
+# confusion = {}
+# confusion[1] = 1 
+# confusion['1'] = 2 
+# confusion[1.0] = 4 
+# sum = 0 
+# for k in confusion:
+#     sum += confusion[k] 
+# print(sum)
+
+#-------------x-------------
+# my_tuple = (1,2,3,4) 
+# my_tuple.append((5,6,7)) 
+# print(len(my_tuple))
+
+# def myfunc(x,y,z,a): 
+#     print(x+y) 
+# nums = [1,2,3,4] 
+# myfunc(*nums)
+
+# class Person: 
+#     def __init__(self, id): 
+#         self.id = id 
+# obama = Person(100) 
+# obama.__dict__['age'] = 49 
+# print(obama.age + len(obama.__dict__)) 
+
