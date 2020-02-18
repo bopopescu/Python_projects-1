865552668407ecd829dbd5682ae62b7b5af08d07
unknown <Raisakulakovska@gmail.com>
2020-02-18 15:12:16 +0300

    youtuber doesnt work yet

--- YoutubeDounloader_30.01.20/start.py
+++ YoutubeDounloader_30.01.20/start.py
@@ -1,24 +1,8 @@
-# import youtube_dl as ud
-# import subprocess
+from lib.Youtube_downloader import start
 
-#https://www.youtube.com/watch?v=fCmm8toLtkA&t=443s
+start()
 
 
-print("===========Youtube Downloader===========")
-choice = int(input("1. Download movie \n2. Download playlist \3Download audio"))
-if choice == 1:
-    url = input("Enter url of moovie")
-elif choice == 2:
-    url = input("Enter url of playlist")
-                
-def youtub_loader():
-    exit = True
-    While not exit:  
-        command = "youtube-dl " + url + " -c"
-        call(command.split(), shell=False)
-        from subprocess import call
-
-youtub_loader()
 
 
 
