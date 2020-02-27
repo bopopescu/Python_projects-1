2a01d34b661c3a842787e4945acb7f6bba745571
Раїса Кулаковська <kulakovska@rivne.itstep.org>
2020-02-27 21:09:02 +0200

    --

--- SQL_Python/DB_manager/start.py
+++ SQL_Python/DB_manager/start.py
@@ -1,6 +1,34 @@
-from lib.db_manager import Db_manager, manager
+from lib.db_manager import Db_manager
 
 
+def manager():
+    """db_manager based on classes"""
+    exit = False
+    while not exit:
+        test = Db_manager("localhost", "root", "", "loginsystem")
+        print("=========== DB Manager ==========")
+        choice = int(input(
+            "1. Register user \n2. Login \n3. Show users \n4. Delete user \n5. Exit \n"
+        ))
+        if choice < 1 or choice > 5:
+            raise Exception("Number of choice must be 1...5!")
+        print("Your choice is: ", choice, "\n")
+        if choice == 1:
+            a = test.register_user()
+            print(a)
+        if choice == 2:
+            res = test.login_user()
+            for item in res:
+                print(item)
+        if choice == 3:
+            res = test.show_users()
+            for item in res:
+                print(item)
+        if choice == 4:
+            test.delete_user()
+        if choice == 5:
+            exit = True
+            print("By!")
 
 try:
     manager()
